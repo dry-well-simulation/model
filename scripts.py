9840c673c0fcae23debfca43900b3dce96fd0787
amir <amirp@breezometer.com>
2021-05-11 10:03:43 +0300

    added next_radius

--- scripts.py
+++ scripts.py
@@ -1,5 +1,46 @@
+from numpy import pi
 import numpy as np
 
+
+def calculate_Q_slice(K_h, hw, zspan, R, r_w, r_initial, PSI):
+    R_correct = R
+    Q_slice = np.zeros_like(zspan)
+    z_below_water = zspan < hw
+
+    if np.any(z_below_water):
+        # stop here
+        if length(idx) > 1
+            idx = idx(1:end - 1);
+            else
+            idx = [];
+
+    return Q_slice, R_correct
+
+
+def next_radius(K_h, K_v, next_hw, hw, zspan, next_R, R, r_w, r_initial, PSI, n, theta_i, dt, temp_R, O_r, eps_R, RK):
+    # todo: use dz instead of zspan[1]
+    Q_h_slice, next_R = calculate_Q_slice(K_h, (next_hw + hw) / 2, zspan, (next_R + R)/ 2, r_w, r_initial, PSI)
+    Q_v_slice = calculate_Q_v_slice(K_v, (next_R + R) / 2, r_w).T
+    next_R = R + (Q_h_slice + Q_v_slice / zspan[1]).T / (2 * pi * ((next_R + R) / 2) * (n-theta_i)) * dt
+    O_r[1] = np.mean(np.abs(next_R - temp_R) / R)
+    if O_r[1] < O_r[0]:
+        O_r[0] = O_r[1]
+    else:
+        while O_r[1] > eps_R and O_r[1] > O_r[0]:
+            temp_R = next_R
+            R_T = R
+            next_R_Q = R + (Q_h_slice + Q_v_slice/ zspan[1]).T / (2 * pi * R * (n-theta_i)) * (dt / RK)
+            for _ in range(RK - 2):
+                R_T = next_R_Q
+                next_R_Q = R_T + (Q_h_slice + Q_v_slice / zspan[1]).T / (2 * pi * ((R_T + next_R_Q) / 2) * (n-theta_i)) * (dt / RK)
+
+            next_R = next_R_Q + (Q_h_slice + Q_v_slice / zspan[1]).T / (2 * pi * ((next_R + R_T) / 2) * (n-theta_i)) * (dt / RK)
+            O_r[1] = np.mean(np.abs(next_R - temp_R) / R)
+        Q_h_slice, next_R = calculate_Q_slice(K_h, (next_hw + hw) / 2, zspan, (next_R + R) / 2, r_w, r_initial, PSI)
+        Q_v_slice = calculate_Q_v_slice(K_v, (next_R + R) / 2, r_w).T
+        next_R = R + (Q_h_slice - Q_v_slice / zspan[1]).T/ (2 * pi * ((next_R + R) / 2) * (n - theta_i)) * dt
+        O_r[0] = np.mean(np.abs(next_R - temp_R) / R)
+    return O_r, next_R
 
 def compute(dt, Qw, A, K_h, K_v, hw, R, r_w, zspan, L_w, n, theta_i, Z, PSI_i, r_initial, eps_R, eps_h, RK):
     """
@@ -40,7 +81,7 @@ def compute(dt, Qw, A, K_h, K_v, hw, R, r_w, zspan, L_w, n, theta_i, Z, PSI_i, r
     while O_r[0] > eps_R or O_h[0] > eps_h:
         temp_R = next_R
         temp_hw = next_hw
-        O_r, next_R = next_radius(K_h, K_v, next_hw, hw, zspan, next_R, R, r_w, r_initial, PSI, n, theta_i, dt,
+        O_r, next_R = next_radius(K_h, K_v, next_hw, hw, zspan, next_R, R, r_w, r_initial, PSI_i, n, theta_i, dt,
                                   temp_R, O_r, eps_R, RK)
 
         O_h, next_hw, next_Q_spill, next_Z, next_zR = \
@@ -52,5 +93,4 @@ def compute(dt, Qw, A, K_h, K_v, hw, R, r_w, zspan, L_w, n, theta_i, Z, PSI_i, r
             O_h[0] = O_h[1]
 
 
-    return None
-    # return next_hw,next_R,next_Q_spill,next_Z,next_zR,dt2
+    return next_hw, next_R, next_Q_spill, next_Z, next_zR, dt
