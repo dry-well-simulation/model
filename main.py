a64f3800ff65a516f8abf58a448f9037d7d4aabe
amir <amirp@breezometer.com>
2021-05-11 15:59:39 +0300

    added plotting

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import numpy as np
 from scripts import compute
+import matplotlib.pyplot as plt
+
 
 EPSILON = 0.01
 EXAGGERATION_FACTOR = 10
@@ -90,18 +92,31 @@ def main(K, L_w, T, r_w, n, theta_i, T_on, Qw_on, I=1, eps_R=0.01, eps_h=0.01, t
     if t_to_show is None:
         t_to_show = np.array([33, 90, 120, 180, 360]) #  times to show in figure
         # todo: make it relative to T_on, not abs values
-        h_w = hw_all
-        Z_vert_fl = np.flip(Z_vert_origin).T  # Todo:remove .T
-        R_vert_fl = r_w * np.ones((time_int, len(t_to_show)))
-        for i, t_to_show_now in enumerate(t_to_show):
-            idx_of_t_to_show_now = np.argwhere(t >= t_to_show_now)[0]  # todo: what if this fails? do something
-            R_vert_fl_tmp = np.flip(R_vertical[:idx_of_t_to_show_now])
-            R_vert_fl[:idx_of_t_to_show_now, i] = R_vert_fl_tmp.T  # not sure this will work
-            R_plot = np.concatenate((R_vert_fl, R_all[:, idx_of_t_to_show_now],))
-            Z_plot = np.concatenate((Z_vert_fl.T, zspan, ))
+    # h_w = hw_all
+    Z_vert_fl = np.flip(Z_vert_origin)
+    R_vert_fl = r_w * np.ones((time_int, len(t_to_show)))
+    R_plot = dict()
+    Z_plot = dict()
+    for i, t_to_show_now in enumerate(t_to_show):
+        idx_of_t_to_show_now = np.argwhere(t >= t_to_show_now)[0][0]  # todo: what if this fails? do something
+        R_vert_fl_tmp = np.flip(R_vertical[:idx_of_t_to_show_now])
+        R_vert_fl[:idx_of_t_to_show_now, i] = R_vert_fl_tmp  # not sure this will work
+        R_plot[t_to_show_now] = np.concatenate((R_vert_fl[:, i], R_all[:, idx_of_t_to_show_now],))
+        Z_plot[t_to_show_now] = np.concatenate((Z_vert_fl.T, zspan, ))
     return R_plot, Z_plot, zspan, hw_all, t
 
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    main(K=5/24/60, L_w=10, T=200, r_w=0.2, n=0.3, theta_i=0.1, T_on=30, Qw_on=30/60)
+    R_plot, Z_plot, zspan, hw_all, t = \
+        main(K=5/24/60, L_w=10, T=400, r_w=0.2, n=0.3, theta_i=0.1, T_on=30, Qw_on=30/60)
+
+    plt.plot(t, hw_all, '.', label='h_w(t)')
+    plt.legend()
+    plt.show()
+
+    for time_ in R_plot.keys():
+        plt.scatter(R_plot[time_], Z_plot[time_], label=time_)
+    plt.legend()
+    plt.show()
+
