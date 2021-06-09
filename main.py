96c29a86e14ce6db96a68c4385288baaf2d7cd8a
amir <amirp@breezometer.com>
2021-06-09 12:20:15 +0300

    started refactoring

--- main.py
+++ main.py
@@ -3,10 +3,10 @@ import matplotlib.pyplot as plt
 
 
 if __name__ == '__main__':
-    # example run to test
+    # example run
     R_plot, Z_plot, zspan, hw_all, t = \
-        core_simulation(K=5/24/60, L_w=10, T=400, r_w=0.2, n=0.3,
-                        theta_i=0.1, T_on=30, Qw_on=30/60, Nt=1000)
+        core_simulation(horiz_cond=5 / 24 / 60, well_length=10, simulation_time=400, well_radius=0.2, porosity=0.3,
+                        initial_VWC=0.1, recharge_duration=30, recharge_rate=30 / 60, n_time_steps=1000)
 
     plt.plot(t, hw_all, '.', label='h_w(t)')
     plt.legend()
